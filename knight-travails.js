856d3898c33a3a52c48bd90a9d5efa56b553e906
John Moorman <45500240+mojoro@users.noreply.github.com>
2024-11-13 19:24:30 +0100

    Clean up code and finalize solution

--- knight-travails.js
+++ knight-travails.js
@@ -1,22 +1,8 @@
 class Node {
-  constructor(vertex = null, moves = null, adjacencyList) {
+  constructor(vertex = null, moves = null) {
     this.vertex = vertex;
     this.parent = null;
     this.moves = moves;
-    this.subNodes = [];
-    this.adjacencyList = adjacencyList;
-  }
-
-  createSubNodes() {
-    for (const vertex of this.moves) {
-      const subNode = new Node(
-        vertex,
-        this.adjacencyList[vertex],
-        this.adjacencyList
-      );
-      subNode.parent = this;
-      this.subNodes.push(subNode);
-    }
   }
 }
 
@@ -73,6 +59,51 @@ class KnightMovesGraph {
     return possibleMoves;
   }
 
+  findPath(startingVertex, targetVertex) {
+    let q = [[startingVertex, null]];
+    let frontIndex = 0;
+    let depth = 0;
+
+    while (frontIndex < q.length) {
+      let front = q[frontIndex];
+      let node = new Node(front[0], this.adjacencyList[front[0]]);
+      if (front[1]) node.parent = front[1];
+
+      if (node.vertex == targetVertex) return this.returnPathToNode(node);
+      else {
+        for (const move of node.moves) {
+          q.push([move, node]);
+        }
+      }
+      frontIndex++;
+    }
+  }
+
+  returnPathToNode(node) {
+    let depth = 0;
+    const pathArray = [];
+    while (node.parent) {
+      depth++;
+      pathArray.push(this.returnCoordsFromVertex(node.vertex));
+      node = node.parent;
+    }
+    pathArray.push(this.returnCoordsFromVertex(node.vertex));
+    pathArray.reverse();
+    return { depth, pathArray };
+  }
+
+  printLevels(vertex, depth) {
+    const node = new Node(vertex, this.adjacencyList[vertex]);
+    if (depth == 0) {
+      return;
+    } else {
+      console.log(node.vertex + " - " + node.moves);
+      for (const move of node.moves) {
+        this.printLevels(move, depth - 1);
+      }
+    }
+  }
+
   checkCoordValidity(coords) {
     if (coords[0] >= 0 && coords[0] <= 7 && coords[1] >= 0 && coords[1] <= 7)
       return true;
@@ -92,56 +123,6 @@ class KnightMovesGraph {
     coords[1] = vertex % 8;
     return coords;
   }
-
-  findPath(startingVertex, targetVertex) {
-    let q = [[startingVertex, null]];
-    let frontIndex = 0;
-    let depth = 0;
-
-    while (frontIndex < q.length) {
-      let front = q[frontIndex];
-      let node = new Node(
-        front[0],
-        this.adjacencyList[front[0]],
-        this.adjacencyList
-      );
-      if (front[1]) node.parent = front[1];
-
-      if (node.vertex == targetVertex) return this.returnDepthFromNode(node);
-      else {
-        for (const move of node.moves) {
-          q.push([move, node]);
-        }
-      }
-      depth++;
-      frontIndex++;
-    }
-  }
-
-  returnDepthFromNode(node) {
-    let depth = 0;
-    while (node.parent) {
-      depth++;
-      node = node.parent;
-    }
-    return depth;
-  }
-
-  printLevels(vertex, depth) {
-    const node = new Node(
-      vertex,
-      this.adjacencyList[vertex],
-      this.adjacencyList
-    );
-    if (depth == 0) {
-      return;
-    } else {
-      console.log(node.vertex + " - " + node.moves);
-      for (const move of node.moves) {
-        this.printLevels(move, depth - 1);
-      }
-    }
-  }
 }
 
 function knightMoves(startingCoords, endingCoords) {
@@ -154,7 +135,11 @@ function knightMoves(startingCoords, endingCoords) {
   const startingVertex = graph.returnVertexFromCoords(startingCoords);
   const endingVertex = graph.returnVertexFromCoords(endingCoords);
   const path = graph.findPath(startingVertex, endingVertex);
-  for (const entry of path) {
+  const depth = path.depth;
+  const pathArray = path.pathArray;
+  console.log(`You made it in ${depth} moves!  Here's your path:`);
+
+  for (const entry of pathArray) {
     console.log(entry);
   }
 }
@@ -167,7 +152,4 @@ function knightMoves(startingCoords, endingCoords) {
 knightMoves([0, 0], [1, 6]);
 const graph = new KnightMovesGraph();
 
-console.log(graph.singleMove(graph.returnCoordsFromVertex(17)));
-console.log(graph.printLevels(0, 2));
-
-console.log(graph.findSinglePath(0, 63));
+knightMoves([0, 0], graph.returnCoordsFromVertex(63));
